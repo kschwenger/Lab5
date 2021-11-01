45c1c70598f6598c70756f823e762a04c7d04963
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-01 22:39:35 +0000

    ede[wf

--- stepper_control.py
+++ stepper_control.py
@@ -7,13 +7,7 @@ dataFromhtml = cgi.FieldStorage()
 Selection = dataFromhtml.getvalue('stepper_submit')
 StepperAngle = dataFromhtml.getvalue('slider')
 
-# consider sending both selection and angle no matter what, and doing if in the background code
-if Selection == Apply_Angle:
-  # save StepperAngle to text file
-  data2send = {"slider":StepperAngle}
-elif Selection == Zero_Stepper:
-  # save zero function to txt file, not angle 
-  data2send = {"selection":Selection}
+data2send = {"selection":Selection, "slider":StepperAngle}
 with open('Lab5.txt', 'w') as f:
   json.dump(data2send,f)
 
