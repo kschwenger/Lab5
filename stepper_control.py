73a25ed496bc6c785af96437373bca300a2aaad1
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-01 23:20:40 +0000

    thingspeak integration

--- stepper_control.py
+++ stepper_control.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python37all
+
 import cgi
 import json
+from urllib.request import urlopen # use to send/receive data
+from urllib.parse import urlencode # use to structure a GET string
 
 # get data from html form
 dataFromhtml = cgi.FieldStorage()
 Selection = dataFromhtml.getvalue('stepper_submit')
 StepperAngle = dataFromhtml.getvalue('slider')
 
+# save data to text file
 data2send = {"stepper_submit":Selection, "slider":StepperAngle}
 with open('Lab5.txt', 'w') as f:
   json.dump(data2send,f)
 
+# send data to thingspeak
+GETparams = {"api_key":R4BNKNSZSPTZ7IXD, 1:StepperAngle}
+GETparams = urlencode(GETparams) # format dict as GET string
+url = "https://api.thingspeak.com/update"
+response = urlopen(url + "?" + GETparams) # send GET request
+status = response.status # display request response
+reason = response.reason # display response reason
+
+# display user interface (same as html) updated with most recent selection
 print('Content-type:text/html\n\n')
 print('<html>')
 
