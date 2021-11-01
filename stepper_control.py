8ba64df4aa77ec534f3aa24951a6381f45939820
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-01 22:44:14 +0000

    edit

--- stepper_control.py
+++ stepper_control.py
@@ -7,27 +7,23 @@ dataFromhtml = cgi.FieldStorage()
 Selection = dataFromhtml.getvalue('stepper_submit')
 StepperAngle = dataFromhtml.getvalue('slider')
 
-data2send = {"selection":Selection, "slider":StepperAngle}
+data2send = {"stepper_submit":Selection, "slider":StepperAngle}
 with open('Lab5.txt', 'w') as f:
   json.dump(data2send,f)
 
-print("""
-
-Content-type:text/html\n\n 
-<html> 
-Select Stepper Angle <br>
-<form action="/cgi-bin/stepper_control.cgi" method="POST">
-  <br>
-  0 deg <input type="range" name="slider" min ="0" max="360" value ="180"/> 360 deg <br>
-  <br>
-  <input type="submit" name="stepper_submit" value="Apply_Angle">
-  <br>
-  <br>
-  Or
-  <br>
-  <br>
-  <input type="submit" name="stepper_submit" value="Zero_Stepper">
-</form>
-</html>
-
-""")
+print('Content-type:text/html\n\n')
+print('<html>') 
+print('Select Stepper Angle <br>')
+print('<form action="/cgi-bin/stepper_control.cgi" method="POST">')
+print('<br>')
+print('0 deg <input type="range" name="slider" min ="0" max="360" value ="180"/> 360 deg <br>')
+print('<br>')
+print('<input type="submit" name="stepper_submit" value="Apply_Angle">')
+print('<br>')
+print('<br>')
+print('Or')
+print('<br>')
+print('<br>')
+print('<input type="submit" name="stepper_submit" value="Zero_Stepper">')
+print('</form>')
+print('</html>')
