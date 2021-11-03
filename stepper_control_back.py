eef3c06d046a91b76bae4c08a6f2f8ba602665cb
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-03 01:34:39 +0000

    asdf

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -20,7 +20,7 @@ while True:
 
   if data['stepper_submit'] == "Apply_Angle":
     # turn stepper to angle using class methods
-    myStepper.goAngle(int(data['slider']), currentangle)
+    myStepper.goAngle(int(data['slider']))
     print("Turning to angle " + data['slider'])
   elif data['stepper_submit'] == "Zero_Stepper":
     # turn stepper until led is blocked using class methods (and reading ADC from PCF class method)
