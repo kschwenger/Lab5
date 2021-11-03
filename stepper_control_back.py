9797d011fd18dd8d1d6b3730dfacd69877addef4
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-03 01:00:50 +0000

    test

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -27,8 +27,9 @@ while True:
     # turn stepper until led is blocked using class methods (and reading ADC from PCF class method)
     pass
   
-  #with open('Lab5.txt', 'w') as f:
-    #json.dump(" ",f)
+data2send = {"stepper_submit":" ", "slider":" "}
+with open('Lab5.txt', 'w') as f:
+  json.dump(data2send,f)
 
   time.sleep(.1)
 
