963f3bfd3d4c5e0b1bd24e1b71e878746ce352c4
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-11-03 02:59:11 +0000

    sdef

--- stepper.py
+++ stepper.py
@@ -74,7 +74,7 @@ class Stepper:
     # halfstep until led is blocked
     GPIO.output(self.led, 1)
     
-    while myADC.read(1) > 0:
-      print(myADC.read(1))
+    while myADC.read(0) > 0:
+      print(myADC.read(0))
       self.__halfstep(1)
       
